4d082966d5b78c07fd4252f458ef2a7d881215ca
rovin76 <rovin7678>
2022-09-23 23:27:08 +0530

    added

---
+++ projects/project.js
@@ -0,0 +1,338 @@
+const Projects = [
+	{
+		name: "Clone of Hokmakeup.com",
+		about:
+			"Hokmakeup providing branded cosmetics products online, makeup kit online. In this project, I was leading my team and majorly worked on Home page, Product page, Signin functionality, Cart page, Wishlist and Payment page.",
+		img: "https://miro.medium.com/max/1000/1*wHGO7qEVTje7VTEizfVJiQ.png",
+		gitrepo: "https://github.com/Ankit-Mishra07/hokmakeup",
+		video: "https://youtu.be/fmRkK9wl4Yk",
+		blog: "https://medium.com/@ankitmi468/cloning-hokmakeup-com-a-functional-and-visual-clone-203d3d4a5e89",
+		tech_stack: "HTML | CSS | JAVASCRIPT",
+		tech_stackicons: [
+			`<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+			`<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+		],
+		project_link: "https://hokmakeup.netlify.app/",
+		project_type: "Team Project",
+		category: ["all", "html"],
+	},
+	{
+		name: "BingStar-MovieApp",
+		about:
+			"A Movie search web application, where user will get category based movies and also can search for a movie. Here user will get all info related to a particular movie with video clips.",
+		img: "https://miro.medium.com/max/700/1*M0MkC_BF8baxFvrN08c3MA.png",
+		gitrepo: "https://github.com/Ankit-Mishra07/BingStar-movie-Search",
+		video: "https://youtu.be/hNrxQDyJYUs",
+		blog: "https://medium.com/@ankitmi468/bingstar-a-movie-search-web-application-70f478a5ee6b",
+		tech_stack: "HTML | CSS | JAVASCRIPT | API's",
+		tech_stackicons: [
+			`<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+			`<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+			`<img src="https://img.icons8.com/nolan/64/api-settings.png" width="48px"/>`,
+		],
+		project_link: "https://bingstar.netlify.app/",
+		project_type: "Indivisual Project",
+		category: ["all", "html"],
+	},
+	{
+		name: "Clone of YouTube",
+		about:
+			"A frontend clone of YouTube, where in the home page user will find popular videos of YouTube and also can search for the desired videos it will show all the videos related to search on the result page.",
+		img: "https://miro.medium.com/max/700/1*nhm4FnPWW69oTAEPT02umQ.png",
+		gitrepo: "https://github.com/Ankit-Mishra07/YouTube",
+		video: "https://youtu.be/EUmdBnQEdB8",
+		blog: "https://medium.com/@ankitmi468/youtube-clone-a-visual-functional-clone-6f50ea8a9757",
+		tech_stack: "HTML | CSS | JAVASCRIPT | API's",
+		tech_stackicons: [
+			`<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+			`<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+			`<img src="https://img.icons8.com/nolan/64/api-settings.png" width="48px"/>`,
+		],
+		project_link: "https://cloning-toutube.netlify.app/",
+		project_type: "Indivisual Project",
+		category: ["all", "html"],
+	},
+	{
+		name: "Clone of Nykaa.com",
+		about:
+			"Nykaa is a beauty retail companythat sells cosmetic commodities online. In this project, I was leading my team and majorly worked on Home page, Product page, Signin functionality, Cart and Wishlist page.",
+		img: "https://user-images.githubusercontent.com/61643245/141407710-c9aa55bc-6997-4cc6-81c7-1a8e18e905a2.png",
+		gitrepo: "https://github.com/Ankit-Mishra07/NykaaCloneWithBackend",
+		video: "https://www.youtube.com/watch?v=jZs-XhFj-GQ&feature=youtu.be",
+		blog: "https://medium.com/@ankitmi468/cloning-of-nykaa-com-a-functional-and-visual-clone-9b3538a1c88d",
+		tech_stack:
+			" HTML | CSS | JavaScript | MongoDB | Express | Embedded JavaScript",
+		tech_stackicons: [
+			`<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+			`<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+			`<img src="https://img.icons8.com/color/48/000000/express.png"/>`,
+			`<img src="https://img.icons8.com/color/48/000000/mongodb.png"/>`,
+			`<img src="https://img.icons8.com/windows/48/000000/node-js.png"/>`,
+		],
+		project_link: "https://nykaaclone.herokuapp.com/",
+		project_type: "Team Project",
+		category: ["all", "html", "other"],
+	},
+	{
+		name: "Clone of Swiggy.com",
+		about:
+			"Swiggy is India’s largest online food ordering and delivery platform. In this project, I was leading my team and majorly worked on Home page, Product page, Signin functionality, Cart and Payment functionality.",
+		img: "https://miro.medium.com/max/700/1*wnLOGfP3w73DKfouzLILFQ.png",
+		gitrepo: "https://github.com/Ankit-Mishra07/Swiggy-Clone",
+		video: "https://youtu.be/onNbtsZUt2k",
+		blog: "https://medium.com/@ankitmi468/cloning-of-swiggy-com-a-functional-and-visual-clone-eeb2df20e699",
+		tech_stack:
+			"HTML | CSS | JAVASCRIPT | React | Redux | MongoDB | Express | Nodejs",
+		tech_stackicons: [
+			`<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+			`<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/react-native.png"/>`,
+			`<img src="https://img.icons8.com/color/48/000000/redux.png"/>`,
+			`<img src="https://img.icons8.com/color/48/000000/mongodb.png"/>`,
+			`<img src="https://img.icons8.com/color/48/000000/express.png"/>`,
+			`<img src="https://img.icons8.com/windows/48/000000/node-js.png"/>`,
+		],
+		project_link: "https://swiggy-clone-five.vercel.app/",
+		project_type: "Team Project",
+		category: ["all", "react", "mern"],
+	},
+	{
+		name: "Quizika Quiz App",
+		about:
+			"Quizika a frontend web application, where user can practice with many categories and difficulty levels of questions.",
+		img: "https://miro.medium.com/max/1400/1*XzwtZudJNaPVMqAnXRbgTQ.png",
+		gitrepo: "https://github.com/Ankit-Mishra07/quizApp",
+		video: "https://youtu.be/qQWBFctJEWA",
+		blog: "https://medium.com/@ankitmi468/quizika-a-quiz-web-application-368b698f4d14",
+		tech_stack: "HTML | CSS | JAVASCRIPT | React | Material UI | Trivia API",
+		tech_stackicons: [
+			`<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+			`<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/react-native.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/material-ui.png"/>`,
+			`<img src="https://img.icons8.com/nolan/64/api-settings.png" width="48px"/>`,
+		],
+		project_link: "https://quiz-app-puce.vercel.app/quiz",
+		project_type: "Indivisual Project",
+		category: ["all", "react"],
+	},
+	{
+		name: "Taskify-Schedule Reminder App",
+		about:
+			"Taskify is a full stack web application. Here user can schedule there daily task with alarm reminder, also can see previous tasks done and user can save important urls for future use and easy to find.",
+		img: "https://miro.medium.com/max/700/1*Rad4ztfe0gXHkhfwZ7YKyw.png",
+		gitrepo: "https://github.com/Ankit-Mishra07/Taskify-schedule-reminder-app",
+		video: "https://youtu.be/Wh-yBt8PJys",
+		blog: "https://medium.com/@ankitmi468/taskify-a-task-scheduler-and-reminder-web-application-3a48918481b6",
+		tech_stack:
+			"HTML | CSS | JAVASCRIPT | React | Mongodb | Express | Nodejs | Mongoose",
+		tech_stackicons: [
+			`<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+			`<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/react-native.png"/>`,
+			`<img src="https://img.icons8.com/color/48/000000/mongodb.png"/>`,
+			`<img src="https://img.icons8.com/color/48/000000/express.png"/>`,
+			`<img src="https://img.icons8.com/windows/48/000000/node-js.png"/>`,
+		],
+		project_link: "https://taskify-kappa.vercel.app/",
+		project_type: "Indivisual Project",
+		category: ["all", "react", "mern"],
+	},
+	{
+		name: "GitHub user search",
+		about:
+			"GitHub user search is a frontend web application. Where user can see their repositories and profile details as well as search for other github user to see their profile details, repositories & many more.",
+		img: "https://miro.medium.com/max/1400/1*Y4IF6czWQkTcuL_mI1j_HQ.png",
+		gitrepo: "https://github.com/Ankit-Mishra07/github-search",
+		video: "https://youtu.be/lE1JdQmyXI0",
+		blog: "https://medium.com/@ankitmi468/github-user-search-a-functional-and-visual-frontend-web-application-1922e42930d4",
+		tech_stack:
+			"HTML | CSS | JAVASCRIPT | React | Redux | Material-UI | GitHub API",
+		tech_stackicons: [
+			`<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+			`<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/react-native.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/material-ui.png"/>`,
+			`<img src="https://img.icons8.com/nolan/64/api-settings.png" width="48px"/>`,
+			`<img src="https://img.icons8.com/color/48/000000/redux.png"/>`,
+		],
+		project_link: "https://github-search-akki.vercel.app/",
+		project_type: "Indivisual Project",
+		category: ["all", "react"],
+	},
+	{
+		name: "Weather Web Application",
+		about:
+			"A web application to show the weather forecast details of 8 days including current day of your, current location detecting feature & here you will be able to search for all the weather details of any city with search suggestions of cities.",
+		img: "https://miro.medium.com/max/1400/1*csbaq7SoEY5nMWtc39LrfQ.png",
+		gitrepo: "https://github.com/Ankit-Mishra07/weather-webApplication",
+		video: "https://youtu.be/MyuOyAf3tmE",
+		blog: "https://medium.com/@ankitmi468/weather-application-a-functional-and-visual-weather-web-app-a010ec060cf0",
+		tech_stack:
+			"React | Redux | Redux-thunk | Open weather api | Google map api | Chakra-ui | React-apexcharts",
+		tech_stackicons: [
+			`<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+			`<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/react-native.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/material-ui.png"/>`,
+			`<img src="https://img.icons8.com/nolan/64/api-settings.png" width="48px"/>`,
+			`<img src="https://img.icons8.com/color/48/000000/redux.png"/>`,
+		],
+		project_link: "https://weather-app-ankit.vercel.app/",
+		project_type: "Indivisual Project",
+		category: ["all", "react"],
+	},
+
+	{
+		name: "One Doc",
+		about:
+			"One Doc — A web application where user can write their important document with lots of editor features. These doc will be saved for future use and can be accessible and editable as well.",
+		img: "../images/ONEDOC.jpg",
+		gitrepo: "https://github.com/Ankit-Mishra07/OneDoc",
+		video: "https://youtu.be/gU-2Tr-o78k",
+		blog: "https://medium.com/@ankitmi468/one-doc-a-functional-and-visual-web-application-d743b1030bd7",
+		tech_stack: "React | Redux | Express | MongoDB | Mongoose | Quill",
+		tech_stackicons: [
+			`<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+			`<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/react-native.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/material-ui.png"/>`,
+			`<img src="https://img.icons8.com/nolan/64/api-settings.png" width="48px"/>`,
+			`<img src="https://img.icons8.com/color/48/000000/redux.png"/>`,
+		],
+		project_link: "https://one-doc.vercel.app",
+		project_type: "Indivisual Project",
+		category: ["all", "react", "mern"],
+	},
+	{
+		name: "Resume Builder",
+		about:
+			"Resume builder is a web application, where user can build and download their resume in pdf format by just filling their personal details. Also user can see preview of resume while filling the data.",
+		img: "../images/resumeBuilder.jpg",
+		gitrepo: "https://github.com/Ankit-Mishra07/resume_builder",
+		video: "https://youtu.be/PXmtsMenTaQ",
+		blog: "https://medium.com/@ankitmi468/resume-builder-a-functional-and-visual-web-application-9f1145a8680",
+		tech_stack:
+			"HTML | CSS | JavaScript | React | Express | MongoDB | Mongoose",
+		tech_stackicons: [
+			`<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+			`<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/react-native.png"/>`,
+			`<img src="https://img.icons8.com/color/50/000000/material-ui.png"/>`,
+			`<img src="https://img.icons8.com/nolan/64/api-settings.png" width="48px"/>`,
+			`<img src="https://img.icons8.com/color/48/000000/redux.png"/>`,
+		],
+		project_link: "https://resumebuilder-ankit.vercel.app",
+		project_type: "Indivisual Project",
+		category: ["all", "react", "mern"],
+	},
+];
+
+let projects__container = document.getElementById("projects--container");
+
+let all_projects = document.querySelector(".all_projects");
+// let react_projects = document.querySelector(".react_projects");
+// let mern_projects = document.querySelector(".mern_projects");
+// let other_projects = document.querySelector(".other_projects");
+// let html_projects = document.querySelector(".html_projects");
+// all_projects.classList.add("active_tech_project");
+
+
+
+// all_projects.addEventListener("click", () => {
+// 	filterProjects("all");
+// 	all_projects.classList.add("active_tech_project");
+// 	react_projects.classList.remove("active_tech_project");
+// 	mern_projects.classList.remove("active_tech_project");
+// 	// other_projects.classList.remove("active_tech_project")
+// 	html_projects.classList.remove("active_tech_project");
+// });
+// react_projects.addEventListener("click", () => {
+// 	filterProjects("react");
+// 	all_projects.classList.remove("active_tech_project");
+// 	react_projects.classList.add("active_tech_project");
+// 	mern_projects.classList.remove("active_tech_project");
+// 	// other_projects.classList.remove("active_tech_project")
+// 	html_projects.classList.remove("active_tech_project");
+// });
+// mern_projects.addEventListener("click", () => {
+// 	filterProjects("mern");
+// 	all_projects.classList.remove("active_tech_project");
+// 	react_projects.classList.remove("active_tech_project");
+// 	mern_projects.classList.add("active_tech_project");
+// 	// other_projects.classList.remove("active_tech_project")
+// 	html_projects.classList.remove("active_tech_project");
+// });
+
+
+
+// other_projects.addEventListener("click", () => {
+//     filterProjects("other");
+//     all_projects.classList.remove("active_tech_project")
+//     react_projects.classList.remove("active_tech_project")
+//     mern_projects.classList.remove("active_tech_project")
+//     // other_projects.classList.add("active_tech_project")
+//     html_projects.classList.remove("active_tech_project")
+// })
+
+// html_projects.addEventListener("click", () => {
+// 	filterProjects("html");
+// 	all_projects.classList.remove("active_tech_project");
+// 	react_projects.classList.remove("active_tech_project");
+// 	mern_projects.classList.remove("active_tech_project");
+// 	// other_projects.classList.remove("active_tech_project")
+// 	html_projects.classList.add("active_tech_project");
+// });
+
+function filterProjects(basis) {
+	if (basis === "all") {
+		let filtered = Projects.filter((el) => {
+			return el.category.includes(basis);
+		});
+		displayProjectData(filtered.reverse());
+	} else {
+		let filtered = Projects.filter((el) => {
+			return el.category.includes(basis);
+		});
+		displayProjectData(filtered);
+	}
+}
+
+function displayProjectData(Projects) {
+	projects__container.innerHTML = null;
+	Projects.reverse().forEach((pro) => {
+		let main = document.createElement("div");
+		main.setAttribute("class", "portfolio-item padd-15");
+		main.innerHTML = `
+    <div class="portfolio-item-inner shadow-dark">
+    <div class="portfolio-img">
+      <img src=${pro.img} alt="">
+      <h3 class="project-name">${pro.name}</h3>
+      <h4 class="project-name project__about">${pro.about}</h4>
+      <div class="navigate_ar">
+      <a href=${pro.project_link} target="_blank" title="Demo" class="see-project"><i class="fas fa-eye"></i></a>
+      <a href=${pro.gitrepo} target="_blank" title="GitHub Repository" class="github-repo"><i class="fab fa-github"></i></a>
+      </div>
+      <div class="project_tech_box">
+      <span class="TechStacks__text">TechStacks: </span> ${pro.tech_stack}
+      </div>
+      
+    </div>
+  </div>
+    `;
+		projects__container.append(main);
+	});
+}
+
+displayProjectData(Projects);
